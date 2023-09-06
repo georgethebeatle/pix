7f54b5f6e6cecf624a5abf01e7241dd2ff7bb204
Georgi Sabev <georgethebeatle@gmail.com>
2023-09-06 12:50:25 +0300

    Make copymeta command skip non-exif files

--- commands/copymeta.go
+++ commands/copymeta.go
@@ -1,10 +1,12 @@
 package commands
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 
 	"github.com/georgethebeatle/pix/commands/helpers"
 	"github.com/urfave/cli/v2"
@@ -54,7 +56,16 @@ func copyMetadata(srcDir, dstDir string) error {
 		srcImagePath := filepath.Join(srcDir, file.Name())
 		dstImagePath := filepath.Join(dstDir, file.Name())
 
-		if err := exec.Command("exiftool", "-overwrite_original", "-tagsFromFile", srcImagePath, dstImagePath).Run(); err != nil {
+		cmdErr := bytes.NewBuffer([]byte{})
+		cmd := exec.Command("exiftool", "-overwrite_original", "-tagsFromFile", srcImagePath, dstImagePath)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = cmdErr
+		if err := cmd.Run(); err != nil {
+			fmt.Printf("err.Error() = %+v\n", err.Error())
+			if strings.Contains(cmdErr.String(), "Unknown file type") {
+				fmt.Printf("Skipping unknown file: %q\n", srcImagePath)
+				continue
+			}
 			return fmt.Errorf("failed to copy exif metadata: %w", err)
 		}
 	}
